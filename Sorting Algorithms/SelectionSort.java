4b6b249a289236e86b249fbe1d474dcd473e0268
Piyush kumar <145029803+Piyushraj7982@users.noreply.github.com>
2024-08-26 23:03:41 +0530

    Update SelectionSort.java

--- Sorting Algorithms/SelectionSort.java
+++ Sorting Algorithms/SelectionSort.java
@@ -1,24 +1,24 @@
-// public class SelectionSort {
-//     public static void main(String[] args) {
-//         int[] arr = { 38, 52, 9, 18, 6, 62, 13 };
-//         int temp = 0;
-//         int min;
-//         for (int i = 0; i < arr.length; i++) {
-//             min = i;
-//             for (int j = i + 1; j < arr.length; j++) {
-//                 if (arr[j] < arr[min]) {
-//                     min = j;
-//                 }
-//             }
-//             temp = arr[i];
-//             arr[i] = arr[min];
-//             arr[min] = temp;
-//         }
-//         for (int i = 0; i < arr.length; i++) {
-//             System.out.print(arr[i] + " ");
-//         }
-//     }
-// }
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = { 38, 52, 9, 18, 6, 62, 13 };
+        int temp = 0;
+        int min;
+        for (int i = 0; i < arr.length; i++) {
+            min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
 
 public class SelectionSort {
     public static void main(String[] args) {

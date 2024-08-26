5da7f3798be1d6b73205b994f1650575b3bee464
Piyush kumar <145029803+Piyushraj7982@users.noreply.github.com>
2024-08-26 22:47:38 +0530

    Update LinearSearch.java

--- Searching Algorithms/LinearSearch.java
+++ Searching Algorithms/LinearSearch.java
@@ -1,40 +1,45 @@
-// public class LinearSearch {
-//     public static void main(String[] args) {
-//         int[] arr = { 1, 2, 3, 4, 5, 6 };
-//         int num = 4;
-//         int i;
-//         for (i = 0; i < arr.length; i++) {
-//             if (arr[i] == num) {
-//                 System.out.println(num + " is found at " + i + " index");
-//                 break;
-//             }
-//         }
-//         if (i == arr.length) {
-//             System.out.println(num + " not found in array");
-//         }
-//     }
-// }
+<h2> For Number </h2>
+```JAVA
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        int num = 4;
+        int i;
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] == num) {
+                System.out.println(num + " is found at " + i + " index");
+                break;
+            }
+        }
+        if (i == arr.length) {
+            System.out.println(num + " not found in array");
+        }
+    }
+}
 
-// public class LinearSearch {
-// public static void main(String[] args) {
-// int[] arr = { 1, 2, 3, 4, 5, 6 };
-// int num = 9;
-// boolean found = false;
+public class LinearSearch {
+    public static void main(String[] args) {
+    int[] arr = { 1, 2, 3, 4, 5, 6 };
+    int num = 9;
+    boolean found = false;
 
-// for (int i = 0; i < arr.length; i++) {
-// if (arr[i] == num) {
-// System.out.println(num + " is found at " + i + " index");
-// found = true;
-// break; // exit the loop once the number is found
-// }
-// }
+    for (int i = 0; i < arr.length; i++) {
+    if (arr[i] == num) {
+    System.out.println(num + " is found at " + i + " index");
+    found = true;
+    break; // exit the loop once the number is found
+    }
+}
 
-// if (!found) {
-// System.out.println(num + " not found in array");
-// }
-// }
-// }
+        if (!found) {
+        System.out.println(num + " not found in array");
+        }
+    }
+}
+```
 
+<h2> For String </h2>
+```JAVA
 public class LinearSearch {
     public static void main(String[] args) {
         String[] arr = { "pk", "ck", "dk", "kk", "rk" };
@@ -51,3 +56,4 @@ public class LinearSearch {
         }
     }
 }
+```

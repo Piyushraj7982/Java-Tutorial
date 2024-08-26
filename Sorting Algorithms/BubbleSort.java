568d9718b6f9ed4081fe101f5408be034098e6c7
Piyush kumar <145029803+Piyushraj7982@users.noreply.github.com>
2024-08-26 23:02:45 +0530

    Update BubbleSort.java

--- Sorting Algorithms/BubbleSort.java
+++ Sorting Algorithms/BubbleSort.java
@@ -1,30 +1,32 @@
-// public class BubbleSort {
-//     public static void main(String[] args) {
-//         int[] arr = { 36, 19, 29, 12, 5 };
-//         int temp = 0;
+```JAVA
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = { 36, 19, 29, 12, 5 };
+        int temp = 0;
 
-//         for (int i = 0; i < arr.length; i++) { // no of round
-//             int flag = 0;
-//             for (int j = 0; j < arr.length - 1 - i; j++) { // no of swap
-//                 if (arr[j] > arr[j + 1]) {
-//                     temp = arr[j];
-//                     arr[j] = arr[j + 1];
-//                     arr[j + 1] = temp;
-//                     flag = 1;
-//                 }
+        for (int i = 0; i < arr.length; i++) { // no of round
+            int flag = 0;
+            for (int j = 0; j < arr.length - 1 - i; j++) { // no of swap
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    flag = 1;
+                }
 
-//             }
-//             if (flag == 0) {
-//                 break;
-//             }
-//         }
-//         for (int i = 0; i < arr.length; i++) {
-//             System.out.print(arr[i] + " ");
-//         }
+            }
+            if (flag == 0) {
+                break;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
 
-//     }
-// }
-
+    }
+}
+```
+```JAVA
 public class BubbleSort {
     public static void main(String[] args) {
         String[] arr = { "pk", "ck", "dk", "kk", "rk" };
@@ -51,3 +53,4 @@ public class BubbleSort {
 
     }
 }
+```
